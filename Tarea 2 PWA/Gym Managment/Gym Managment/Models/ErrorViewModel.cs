5df15a62739806c545c3e3641ef098b7c105a670
AlvinRHD <ezequielhernandes907@gmail.com>
2024-02-22 20:26:46 -0600

    Eliminando carpeta tarea 2 PWA, version anterior del trabajo

--- Tarea 2 PWA/Gym Managment/Gym Managment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gym_Managment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
