aa628116b419cdff66e55d0eb9674f1e514416af
Rciat05 <rciglesiaa05@gmail.com>
2024-02-25 12:11:34 -0600

    Agregando contenido al repositorio customer

--- Gym_ManagamentDemo/Gym_Management/Repositories/Customer/CustomerRepository.cs
+++ Gym_ManagamentDemo/Gym_Management/Repositories/Customer/CustomerRepository.cs
@@ -1,18 +1,27 @@
-﻿using Gym_Management.Models;
+﻿using Gym_Management.Data;
+using Gym_Management.Models;
 
 namespace Gym_Management.Repositories.Customer
 {
-    public class CustomerRepository
+    public class CustomerRepository : ICustomerRepository
     {
+        private readonly SqlDataAccess _dbConnection;
+
+        public CustomerRepository(SqlDataAccess dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
 
         public IEnumerable<CustomerModel> GetAll() 
         {
+            List<CustomerModel> Customerlist = new List<CustomerModel>();
+
+            using (var connection = _dbConnection.GetConnection())
+            {
+
+            }
 
         }
 
-        public void Add(CustomerModel customer) 
-        {
-
-        }
     }
 }
